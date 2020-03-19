460ee9b2976160b067e39589e347ff5eef236469
NOOBDY <htommyf12@gmail.com>
2020-03-19 21:56:17 +0800

    added high low close chart

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,9 @@ class Data {
     Map data = json['Time Series (Daily)'];
 
     List<ChartData> open = List();
+    List<ChartData> high = List();
+    List<ChartData> low = List();
+    List<ChartData> close = List();
 
     data.forEach(
       (key, value) {
@@ -40,16 +43,66 @@ class Data {
             double.parse(value['1. open']),
           ),
         );
+        high.add(
+          new ChartData(
+            new DateTime(
+              int.parse(key.substring(0, 4)),
+              int.parse(key.substring(5, 7)),
+              int.parse(key.substring(8)),
+            ),
+            double.parse(value['2. high']),
+          ),
+        );
+        low.add(
+          new ChartData(
+            new DateTime(
+              int.parse(key.substring(0, 4)),
+              int.parse(key.substring(5, 7)),
+              int.parse(key.substring(8)),
+            ),
+            double.parse(value['3. low']),
+          ),
+        );
+        close.add(
+          new ChartData(
+            new DateTime(
+              int.parse(key.substring(0, 4)),
+              int.parse(key.substring(5, 7)),
+              int.parse(key.substring(8)),
+            ),
+            double.parse(value['4. close']),
+          ),
+        );
       },
     );
-    return new Data(seriesList: [
-      charts.Series<ChartData, DateTime>(
-        id: 'test',
-        domainFn: (ChartData data, _) => data.time,
-        measureFn: (ChartData data, _) => data.val,
-        data: open,
-      )
-    ]);
+    return new Data(
+      seriesList: [
+        charts.Series<ChartData, DateTime>(
+          id: 'open',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: open,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'high',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: high,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'low',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: low,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'close',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: close,
+        ),
+      ],
+    );
   }
 }
 
@@ -78,21 +131,29 @@ class _AppState extends State<App> {
       title: 'Test',
       home: Scaffold(
         body: Center(
-          child: FutureBuilder<Data>(
-            future: futureData,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<charts.Series<dynamic, DateTime>> _data =
-                    snapshot.data.seriesList;
-                return charts.TimeSeriesChart(
-                  _data,
-                  animate: false,
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.hasError}');
-              }
-              return CircularProgressIndicator();
-            },
+          child: SizedBox(
+            child: FutureBuilder<Data>(
+              future: futureData,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<charts.Series<dynamic, DateTime>> _data =
+                      snapshot.data.seriesList;
+                  return charts.TimeSeriesChart(
+                    _data,
+                    animate: false,
+                    primaryMeasureAxis: new charts.NumericAxisSpec(
+                      tickProviderSpec: new charts.BasicNumericTickProviderSpec(
+                        zeroBound: false,
+                      ),
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.hasError}');
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+            height: 400,
           ),
         ),
         backgroundColor: Colors.white,
