f4c4bc0e99a01f93b9b7a4b4dfae25183bb62e04
Tommy Huang <htommyf12@gmail.com>
2020-03-20 22:54:16 +0800

    file tree restructure

---
+++ lib/data.dart
@@ -0,0 +1,116 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<Data> fetchData(String apiKey, String symbol) async {
+  final response = await http.get(
+      'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=$apiKey');
+
+  if (response.statusCode == 200) {
+    return Data.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed');
+  }
+}
+
+class Data {
+  List<charts.Series<dynamic, DateTime>> seriesList;
+
+  Data({
+    this.seriesList,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    Map data = json['Time Series (Daily)'];
+
+    List<ChartData> open = List();
+    List<ChartData> high = List();
+    List<ChartData> low = List();
+    List<ChartData> close = List();
+
+    int counter = 0;
+
+    data.forEach(
+      (key, value) {
+        if (counter < 30) {
+          open.add(
+            new ChartData(
+              new DateTime(
+                int.parse(key.substring(0, 4)),
+                int.parse(key.substring(5, 7)),
+                int.parse(key.substring(8)),
+              ),
+              double.parse(value['1. open']),
+            ),
+          );
+          high.add(
+            new ChartData(
+              new DateTime(
+                int.parse(key.substring(0, 4)),
+                int.parse(key.substring(5, 7)),
+                int.parse(key.substring(8)),
+              ),
+              double.parse(value['2. high']),
+            ),
+          );
+          low.add(
+            new ChartData(
+              new DateTime(
+                int.parse(key.substring(0, 4)),
+                int.parse(key.substring(5, 7)),
+                int.parse(key.substring(8)),
+              ),
+              double.parse(value['3. low']),
+            ),
+          );
+          close.add(
+            new ChartData(
+              new DateTime(
+                int.parse(key.substring(0, 4)),
+                int.parse(key.substring(5, 7)),
+                int.parse(key.substring(8)),
+              ),
+              double.parse(value['4. close']),
+            ),
+          );
+          counter++;
+        }
+      },
+    );
+    return new Data(
+      seriesList: [
+        charts.Series<ChartData, DateTime>(
+          id: 'open',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: open,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'high',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: high,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'low',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: low,
+        ),
+        charts.Series<ChartData, DateTime>(
+          id: 'close',
+          domainFn: (ChartData data, _) => data.time,
+          measureFn: (ChartData data, _) => data.val,
+          data: close,
+        ),
+      ],
+    );
+  }
+}
+
+class ChartData {
+  final DateTime time;
+  final double val;
+
+  ChartData(this.time, this.val);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,11 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
+import 'data.dart';
 
-Future<Data> fetchData() async {
-  final response = await http.get(
-      'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo');
-
-  if (response.statusCode == 200) {
-    return Data.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed');
-  }
-}
-
-class Data {
-  List<charts.Series<dynamic, DateTime>> seriesList;
-
-  Data({
-    this.seriesList,
-  });
-
-  factory Data.fromJson(Map<String, dynamic> json) {
-    Map data = json['Time Series (Daily)'];
-
-    List<ChartData> open = List();
-    List<ChartData> high = List();
-    List<ChartData> low = List();
-    List<ChartData> close = List();
-
-    data.forEach(
-      (key, value) {
-        open.add(
-          new ChartData(
-            new DateTime(
-              int.parse(key.substring(0, 4)),
-              int.parse(key.substring(5, 7)),
-              int.parse(key.substring(8)),
-            ),
-            double.parse(value['1. open']),
-          ),
-        );
-        high.add(
-          new ChartData(
-            new DateTime(
-              int.parse(key.substring(0, 4)),
-              int.parse(key.substring(5, 7)),
-              int.parse(key.substring(8)),
-            ),
-            double.parse(value['2. high']),
-          ),
-        );
-        low.add(
-          new ChartData(
-            new DateTime(
-              int.parse(key.substring(0, 4)),
-              int.parse(key.substring(5, 7)),
-              int.parse(key.substring(8)),
-            ),
-            double.parse(value['3. low']),
-          ),
-        );
-        close.add(
-          new ChartData(
-            new DateTime(
-              int.parse(key.substring(0, 4)),
-              int.parse(key.substring(5, 7)),
-              int.parse(key.substring(8)),
-            ),
-            double.parse(value['4. close']),
-          ),
-        );
-      },
-    );
-    return new Data(
-      seriesList: [
-        charts.Series<ChartData, DateTime>(
-          id: 'open',
-          domainFn: (ChartData data, _) => data.time,
-          measureFn: (ChartData data, _) => data.val,
-          data: open,
-        ),
-        charts.Series<ChartData, DateTime>(
-          id: 'high',
-          domainFn: (ChartData data, _) => data.time,
-          measureFn: (ChartData data, _) => data.val,
-          data: high,
-        ),
-        charts.Series<ChartData, DateTime>(
-          id: 'low',
-          domainFn: (ChartData data, _) => data.time,
-          measureFn: (ChartData data, _) => data.val,
-          data: low,
-        ),
-        charts.Series<ChartData, DateTime>(
-          id: 'close',
-          domainFn: (ChartData data, _) => data.time,
-          measureFn: (ChartData data, _) => data.val,
-          data: close,
-        ),
-      ],
-    );
-  }
-}
+String apiKey = 'TT1KDKHC0BDTBO6A';
+String symbol = 'GOOGL';
 
 void main() => runApp(App());
 
@@ -121,7 +22,7 @@ class _AppState extends State<App> {
   @override
   void initState() {
     super.initState();
-    futureData = fetchData();
+    futureData = fetchData(apiKey, symbol);
   }
 
   @override
@@ -130,6 +31,19 @@ class _AppState extends State<App> {
     return MaterialApp(
       title: 'Test',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '$symbol',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40,
+              fontFamily: 'Product Sans',
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
         body: Center(
           child: FutureBuilder<Data>(
             future: futureData,
@@ -160,11 +74,4 @@ class _AppState extends State<App> {
       ),
     );
   }
-}
-
-class ChartData {
-  final DateTime time;
-  final double val;
-
-  ChartData(this.time, this.val);
 }
