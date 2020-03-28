d27ed251ee1f41e416712ef082ec65f224274cf4
Tommy Huang <htommyf12@gmail.com>
2020-03-28 20:25:19 +0800

    restructure materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,33 @@ import 'data.dart';
 String apiKey = 'TT1KDKHC0BDTBO6A';
 String symbol = 'GOOGL';
 
-void main() => runApp(FutureWidget());
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
-      home: Scaffold(),
+      title: 'Test',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '$symbol',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontFamily: 'Product Sans',
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(
+          child: FutureWidget(),
+        ),
+        backgroundColor: Colors.grey[900],
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -35,49 +56,29 @@ class FutureState extends State<FutureWidget> {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
-    return MaterialApp(
-      title: 'Test',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '$symbol',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 40,
-              fontFamily: 'Product Sans',
+    return FutureBuilder<Data>(
+      future: futureData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return SizedBox(
+            child: charts.TimeSeriesChart(
+              snapshot.data.seriesList,
+              animate: false,
+              primaryMeasureAxis: new charts.NumericAxisSpec(
+                tickProviderSpec: new charts.BasicNumericTickProviderSpec(
+                  zeroBound: false,
+                ),
+              ),
             ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        body: Center(
-          child: FutureBuilder<Data>(
-            future: futureData,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return SizedBox(
-                  child: charts.TimeSeriesChart(
-                    snapshot.data.seriesList,
-                    animate: false,
-                    primaryMeasureAxis: new charts.NumericAxisSpec(
-                      tickProviderSpec: new charts.BasicNumericTickProviderSpec(
-                        zeroBound: false,
-                      ),
-                    ),
-                  ),
-                  height: 400,
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.hasError}');
-              }
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-        backgroundColor: Colors.grey[900],
-      ),
+            height: 400,
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.hasError}');
+        }
+        return CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        );
+      },
     );
   }
 }
